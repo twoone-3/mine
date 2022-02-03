c9326ded272d1833ea776b4058b043b7a7df86ec
twoone3 <3197653242@qq.com>
2022-02-03 21:54:12 +0800

    修复问题

--- mod/Main.cpp
+++ mod/Main.cpp
@@ -8,10 +8,10 @@
 
 using namespace std;
 
-static vector<pair<Block**, size_t>> g_blocks;
-static size_t g_weight_sum = 0;
-constexpr const char* CONFIG_PATH = "./plugins/mine.json";
 Logger logger("mine");
+vector<pair<Block**, size_t>> g_blocks;
+size_t g_weight_sum = 0;
+constexpr const char* CONFIG_PATH = "./plugins/mine.json";
 
 void entry() {
 	srand(static_cast<unsigned>(time(0)));
@@ -26,21 +26,6 @@ void entry() {
 		{"LapisOre", 10},
 		{"DiamondOre", 2},
 		});
-	//ifstream file(CONFIG_PATH);
-	//if (!file) {
-	//	logger.error("mine.json wasn't found, it will be created automatically");
-	//	ofstream newfile(CONFIG_PATH);
-	//	newfile << config.dump(4);
-	//	newfile.close();
-	//}
-	//else {
-	//	try {
-	//		config = fifo_json::parse(file);
-	//	}
-	//	catch (const std::exception& e) {
-	//		cerr << e.what() << endl;
-	//	}
-	//}
 	for (auto& [key, val] : cm.getJson().items()) {
 		//未初始化，取值为nullptr，需要运行时再取
 		string sym("?m" + key + "@VanillaBlocks@@3PEBVBlock@@EB");
@@ -68,12 +53,13 @@ THook(void, "?solidify@LiquidBlock@@IEBAXAEAVBlockSource@@AEBVBlockPos@@1@Z",
 	uintptr_t _this, BlockSource& bs, BlockPos& bp1, BlockPos& bp2) {
 	//生成石头
 	original(_this, bs, bp1, bp2);
-	short bid = bs.getBlock(bp2).getLegacyBlock().getBlockItemId();
+	short bid = bs.getBlock(bp1).getLegacyBlock().getBlockItemId();
 	if (bid == 4 || bid == 1) {//Stone and Cobblestone
 		size_t random_number = rand() % g_weight_sum;
 		for (auto [block, weight] : g_blocks) {
 			if (random_number < weight) {
-				bs.setBlockSimple(bp2, **block);
+				bs.setBlockSimple(bp1, **block);
+				bs.updateNeighborsAt(bp1);
 				break;
 			}
 			random_number -= weight;
